b60e7e8fa3942d75941bf1c1a5a2285269f56633
arunhistory <garigarimegane625@gmail.com>
2026-08-23 13:01:44 +0900

    Connect FAQ from roadmap page

--- roadmap.js
+++ roadmap.js
@@ -1,6 +1,12 @@
 const menuButton=document.querySelector('[data-menu-button]');
 const mobileMenu=document.querySelector('[data-mobile-menu]');
 
+const faqLinks=[...document.querySelectorAll('a')].filter(link=>link.textContent.trim()==='FAQ');
+faqLinks.forEach(link=>{
+  link.href='./faq.html';
+  link.removeAttribute('data-placeholder-link');
+});
+
 const closeMenu=()=>{
   menuButton?.classList.remove('is-open');
   mobileMenu?.classList.remove('is-open');
