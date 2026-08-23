c43219acbfe454da89b49fc14bea6143444a6b7a
arunhistory <garigarimegane625@gmail.com>
2026-08-23 13:01:36 +0900

    Connect FAQ from crowdfunding page

--- crowdfunding.js
+++ crowdfunding.js
@@ -7,6 +7,12 @@ roadmapLinks.forEach(link=>{
   link.removeAttribute('data-placeholder-link');
 });
 
+const faqLinks=[...document.querySelectorAll('a')].filter(link=>link.textContent.trim()==='FAQ');
+faqLinks.forEach(link=>{
+  link.href='./faq.html';
+  link.removeAttribute('data-placeholder-link');
+});
+
 const closeMenu=()=>{
   menuButton?.classList.remove('is-open');
   mobileMenu?.classList.remove('is-open');
